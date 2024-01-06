8955d7307b0647a60f739662fadd74a02c759bea
sangbok <fx.isbok99@gmail.com>
2024-01-06 23:49:33 +0900

    add inherited convert unit class

--- 240106/exercise_240106.py
+++ 240106/exercise_240106.py
@@ -1,1 +1,25 @@
-print('a')
+"""
+class convert_unit():
+    def __init__(self):
+        self.result = 0
+    def radians(self,i):
+        import math
+        self.result = i * math.pi/180
+        return self.result
+    def degree(self,i):
+        import math
+        self.result = i * 180/math.pi
+        return self.result
+
+
+class inherit_cu(convert_unit):
+    def inch(self,i):
+        self.result = i*39.37
+        return self.result
+    def meter(self,i):
+        self.result = i/39.37
+        return self.result
+
+a = inherit_cu()
+print(a.inch(10))
+"""
